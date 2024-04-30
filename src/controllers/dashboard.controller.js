bebe1d2506bde10b3a901a7316521c849270bf40
unique011 <harshitsharma.2021cs@technonjr.org>
2024-05-01 00:21:13 +0530

    create dashboard function

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,23 +1,65 @@
 import mongoose from "mongoose";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
 import { Video } from "../models/video.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponce } from "../utils/ApiResponce.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
-  //     TODO: Get the channel stats like , total subscribers, total videos, total likes etc.
-    const videoLikeAndView = await Video.aggregate([
-      {
-        $match: {
-          owner: new mongoose.Schema.Types.ObjectId(req.user._id),
-        },
-        
+  const stats = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id),
       },
-      {
-        $addFields: {
-          totalViews: {$sum:"$views"}
-        }
-      }
-    ])
-  return res.status(200);
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "_id",
+        foreignField: "owner",
+        as: "video",
+        pipeline: [
+          {
+            $lookup: {
+              from: "likes",
+              localField: "_id",
+              foreignField: "video",
+              as: "likes",
+            },
+          },
+
+          {
+            $group: {
+              _id: null,
+              totalViews: { $sum: "$views" },
+              totalLikes: { $sum: 1 },
+            },
+          },
+        ],
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      $addFields: {
+        totalSubscribers: {
+          $size: "$subscribers",
+        },
+        totalLikes: { $first: "$video.totalLikes" },
+        totalViews: { $first: "$video.totalViews" },
+      },
+    },
+  ]);
+  if (!stats.length) throw new ApiError(500, "stats can not calculated");
+  return res
+    .status(200)
+    .json(new ApiResponce(200, stats[0], "stats fetch successfully"));
 });
 const getChannelVideos = asyncHandler(async (req, res) => {
   const videos = await Video.aggregate([
