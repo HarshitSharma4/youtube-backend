310c3ca9f81c1a060dc252976842d248d1f38dec
harshitSharma4 <harshit.sharma212002@gmail.com>
2024-08-14 22:27:33 +0530

    load previous config

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 import dotenv from "dotenv";
 import connectDb from "./db/index.js";
 import { app } from "./app.js";
-import path from 'path';
-import express from 'express';
-import { fileURLToPath } from 'url';
+// import path from 'path';
+// import express from 'express';
+// import { fileURLToPath } from 'url';
 
 dotenv.config({
   path: "./.env",
 });
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename); 
-connectDb();
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename); 
+ connectDb();
 app.on("error", (error) => {
   console.log("ERR: ", error);
   throw error;
 });
 
-app.use(express.static(path.join(__dirname,"../public/dist")))
-app.get("*",(req,res)=>{
-  res.sendFile(path.join(__dirname,"../public/dist/index.html")) 
-});
-app.listen(process.env.PORT, () => {
-  console.log(`App is listen on port${process.env.PORT}`);
-});
+// app.use(express.static(path.join(__dirname,"../public/dist")))
+// app.get("*",(req,res)=>{
+//   res.sendFile(path.join(__dirname,"../public/dist/index.html")) 
+// });
+// app.listen(process.env.PORT, () => {
+//   console.log(`App is listen on port${process.env.PORT}`);
+// });
 /*
 import {DB_NAME} from "./constant.js";
 first approach for database connection
